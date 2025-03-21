26604bc3edf135a78950f21a261a9904a2c6443a
Giovanni <giovannirojas153@gmail.com>
2025-03-21 23:08:28 +0000

    aaa

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let mensajes = ["¡Hola!", "Espero que tengas un buen día!", "Sigue adelante!"]
 let indice = 0;
 
 function mostrarMensaje() {
-    let mensajeDiv = document.getElementById("mensaje");
-    mensajeDiv.innerText = mensajes[indice];
-    mensajeDiv.classList.add("mostrar");
-    indice = (indice + 1) % mensajes.length;
+    let mensaje = document.getElementById("mensaje");
+    if (mensaje.style.display === "none" || mensaje.style.display === "") {
+        mensaje.style.display = "block";
+    } else {
+        mensaje.style.display = "none";
+    }
 }
