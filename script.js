7a4affb6b69cbd5c5aee4392f916f8d282df3fe1
Giovanni <giovannirojas153@gmail.com>
2025-03-21 23:30:43 +0000

    ad

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let mensajes = [
     "Hola mi amor, quería ser un poco 'único' para darte una flor por este día",
     "Te doy esta flor con todo mi corazón, y la próxima vez si te quiero dar una buena flor por un día especial en un futuro... ya verás",
     "Así que he diseñado una página web solo para darte este mensaje, aunque sé que es a través de una pantalla y no una flor en persona",
-    "Te quiero muchísimo, mi persona especial. Eres única ❤️"
-    "<img src='flor.jpeg' alt='Una flor para ti' style='width: 150px; height: auto;'>",
+      "Te quiero muchísimo, mi persona especial. Eres única ❤️",
+    "<img src='flor.jpeg' alt='Una flor para ti' style='width: 150px; height: auto;'>"
 ];
 
 let indice = 0;
