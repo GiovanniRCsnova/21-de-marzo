3045373d8c55882a040944cec4811991a08d82ec
Giovanni <giovannirojas153@gmail.com>
2025-03-21 23:26:29 +0000

    aa

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let mensajes = [
-    "Hola mi amor, quería ser un poco 'único' para darte una flor por este día",
-    "Así que he diseñado una página web solo para darte este mensaje, aunque sé que es a través de una pantalla y no una flor en persona",
-    "Te doy esta flor con todo mi corazón, y la próxima vez si te quiero dar una buena flor por un día especial en un futuro... ya verás",
-    "<img src='flor.jpeg' alt='Una flor para ti' style='width: 100px; height: auto;'>",
-    "Te quiero muchísimo, mi persona especial. Eres única ❤️"
+    "1. Hola mi amor, quería ser un poco 'único' para darte una flor por este día",
+    "2. Así que he diseñado una página web solo para darte este mensaje, aunque sé que es a través de una pantalla y no una flor en persona",
+    "3. Te doy esta flor con todo mi corazón, y la próxima vez si te quiero dar una buena flor por un día especial en un futuro... ya verás",
+    "4. <img src='flor.jpeg' alt='Una flor para ti' style='width: 150px; height: auto;'>",
+    "5. Te quiero muchísimo, mi persona especial. Eres única ❤️"
 ];
 
 let indice = 0;
@@ -11,7 +11,7 @@ let indice = 0;
 function mostrarMensaje() {
     let mensaje = document.getElementById("mensaje");
 
-    // Asigna el mensaje o imagen
+    // Asigna el mensaje o la imagen
     mensaje.innerHTML = mensajes[indice];
 
     // Muestra el mensaje si está oculto
